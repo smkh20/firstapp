7fe639a463127a2652f19463cfe66bd28d520724
smkh <mmm.smkh.mmm@gmail.com>
2022-07-25 10:35:13 +0430

    delete enter that was import from action but didnt use

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import { enter, getdata } from "./action";
+import { getdata } from "./action";
 const Home = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
