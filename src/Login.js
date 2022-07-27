446ef503e68ff4f4208cd8fda4f6366d2a033bf8
smkh <mmm.smkh.mmm@gmail.com>
2022-07-27 10:29:34 +0430

    if input was empty and login button pressed focus

--- src/Login.js
+++ src/Login.js
@@ -16,14 +16,18 @@ const Login = () => {
   return (
     <div style={{ display: "flex", flexDirection: "column", width: "50vw", margin: "auto" }}>
       <label>email:</label>
-      <input type="text" onChange={(e) => setemail(e.target.value)} />
+      <input id="email" type="text" onChange={(e) => setemail(e.target.value)} />
       <label>pass:</label>
-      <input type="password" onChange={(e) => setpass(e.target.value)} />
+      <input id="pass" type="password" onChange={(e) => setpass(e.target.value)} />
       <button
         style={{ margin: "5px 0px" }}
         onClick={() => {
           if (email && pass) {
             dispatch(login(email, pass));
+          } else if (!email) {
+            document.getElementById("email").focus();
+          } else {
+            document.getElementById("pass").focus();
           }
         }}
       >
