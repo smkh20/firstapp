1119eeb654c34a9668cbab96f285c96c59fc4a3e
smkh <mmm.smkh.mmm@gmail.com>
2022-07-27 10:08:23 +0430

    debug (check email and pass dont disapper)

--- src/Login.js
+++ src/Login.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { login } from "./action";
@@ -7,15 +7,26 @@ const Login = () => {
   const [email, setemail] = useState("");
   const [pass, setpass] = useState("");
   const dispatch = useDispatch();
-  const token=useSelector((state)=>state.login)
-  const flaglogin=useSelector((state)=>state.flaglogin)
+  const token = useSelector((state) => state.login);
+  const flaglogin = useSelector((state) => state.flaglogin);
+  useEffect(() => {
+    dispatch({ type: "login", payload: false });
+  }, []);
+
   return (
-    <div style={{ display: "flex", flexDirection: "column",width:"50vw",margin:"auto" }}>
+    <div style={{ display: "flex", flexDirection: "column", width: "50vw", margin: "auto" }}>
       <label>email:</label>
       <input type="text" onChange={(e) => setemail(e.target.value)} />
       <label>pass:</label>
       <input type="password" onChange={(e) => setpass(e.target.value)} />
-      <button style={{ margin: "5px 0px" }} onClick={() =>{if(email&&pass){dispatch(login(email, pass))}} }>
+      <button
+        style={{ margin: "5px 0px" }}
+        onClick={() => {
+          if (email && pass) {
+            dispatch(login(email, pass));
+          }
+        }}
+      >
         login
       </button>
       <button onClick={() => navigate("/sign")}>signup</button>
